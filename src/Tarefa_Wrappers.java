e15cfdd4e865f4a9c64618213b298f0682762740
Alexandre Ferreira <alexandre1984do@gmail.com>
2023-10-31 10:12:23 -0300

    Tarefa_Wrappers

---
+++ src/Tarefa_Wrappers.java
@@ -0,0 +1,22 @@
+public class Tarefa_Wrappers {
+
+    public static void main(String args[]){
+
+        wrappers();
+
+    }
+
+    public static void wrappers(){
+        int nota1=10;
+        System.out.println(nota1);
+
+        long nota2=nota1;
+        System.out.println(nota2);
+
+        short nota3=(short)nota1;
+        System.out.println(nota2);
+
+
+    }
+}
+
